60ac1e7cc5977e1472188c9b52a258617ae8306f
flowlysoftware-commits <flowlysoftware@gmail.com>
2026-07-05 19:51:13 +0200

    Update page.tsx

--- app/developer/page.tsx
+++ app/developer/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useMemo, useState, type FormEvent } from "react";
+import { useEffect, useMemo, useState, type FormEvent, type ReactNode } from "react";
 import {
   Activity,
   ArrowRight,
