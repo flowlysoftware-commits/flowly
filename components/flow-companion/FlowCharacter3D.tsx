2a74d631a9648df628a31bd359dd3c3feda094a6
flowlysoftware-commits <flowlysoftware@gmail.com>
2026-07-17 02:26:48 +0200

    Delete components/flow-companion directory

--- components/flow-companion/FlowCharacter3D.tsx
+++
@@ -1,3 +0,0 @@
-"use client";
-export { default } from "@/components/flow-engine/FlowCharacter";
-export type { FlowMode as FlowCharacterMode, FlowFacing as FlowCharacterFacing, FlowEmotion as FlowEmotionSnapshot } from "@/components/flow-engine/types";
